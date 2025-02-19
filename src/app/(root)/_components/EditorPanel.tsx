b85a7238036d9ccc70fccd960a357a037377414c
Sayantan Nandi <officialsayantannandi@gmail.com>
2025-02-20 01:30:37 +0530

    Deployment retrying - 3

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -84,47 +84,44 @@ function EditorPanel() {
             >
               <RotateCcwIcon className="size-4 text-gray-400" />
             </motion.button>
-
-            
           </div>
         </div>
 
         {/* Editor  */}
         <div className="relative group rounded-xl overflow-hidden ring-1 ring-white/[0.05]">
-            <Editor
-              height="600px"
-              language={LANGUAGE_CONFIG[language].monacoLanguage}
-              onChange={handleEditorChange}
-              theme={theme}
-              beforeMount={defineMonacoThemes}
-              onMount={(editor) => setEditor(editor)}
-              options={{
-                minimap: { enabled: false },
-                fontSize,
-                automaticLayout: true,
-                scrollBeyondLastLine: false,
-                padding: { top: 16, bottom: 16 },
-                renderWhitespace: "selection",
-                fontFamily: '"Fira Code", "Cascadia Code", Consolas, monospace',
-                fontLigatures: true,
-                cursorBlinking: "smooth",
-                smoothScrolling: true,
-                contextmenu: true,
-                renderLineHighlight: "all",
-                lineHeight: 1.6,
-                letterSpacing: 0.5,
-                roundedSelection: true,
-                scrollbar: {
-                  verticalScrollbarSize: 8,
-                  horizontalScrollbarSize: 8,
-                },
-              }}
-            />
-
-          {/* <EditorPanelSkeleton /> */}
+          <Editor
+            height="600px"
+            language={LANGUAGE_CONFIG[language].monacoLanguage}
+            onChange={handleEditorChange}
+            theme={theme}
+            beforeMount={defineMonacoThemes}
+            onMount={(editor) => setEditor(editor)}
+            options={{
+              minimap: { enabled: false },
+              fontSize,
+              automaticLayout: true,
+              scrollBeyondLastLine: false,
+              padding: { top: 16, bottom: 16 },
+              renderWhitespace: "selection",
+              fontFamily: '"Fira Code", "Cascadia Code", Consolas, monospace',
+              fontLigatures: true,
+              cursorBlinking: "smooth",
+              smoothScrolling: true,
+              contextmenu: true,
+              renderLineHighlight: "all",
+              lineHeight: 1.6,
+              letterSpacing: 0.5,
+              roundedSelection: true,
+              scrollbar: {
+                verticalScrollbarSize: 8,
+                horizontalScrollbarSize: 8,
+              },
+            }}
+          />
         </div>
       </div>
     </div>
   );
 }
+
 export default EditorPanel;
